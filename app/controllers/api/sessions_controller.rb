55c66e840a414e0e63db8953ab678a16c6693d5a
chithra <chithra.venkatesan.5@gmail.com>
2017-04-18 14:33:45 -0400

    Create API Utils(tested)

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,8 +1,8 @@
-class Api::SessionController < ApplicationController
+class Api::SessionsController < ApplicationController
 
   def create
     @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
-  
+
     if @user
       login(@user)
       render 'api/users/show'
