9589b983cc75115b0150a92cb89cd334709f08e6
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-04-18 15:14:39 -0400

    Add session actions

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,31 @@
+import * as SessionAPIUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+
+
+const receiveCurrentUser = currentUser => ({
+  action: RECEIVE_CURRENT_USER,
+  currentUser
+});
+const receiveErrors = errors => ({
+  action: RECEIVE_ERRORS,
+  errors
+});
+
+export const login = user => dispatch => {
+  return SessionAPIUtil.login(user)
+    .then((res) => dispatch(receiveCurrentUser(res)),
+      (err) => dispatch(receiveErrors(err.responseJSON)));
+};
+
+export const logout = () => dispatch => {
+  return SessionAPIUtil.logout()
+    .then((res) => dispatch(receiveCurrentUser(null)));
+};
+
+export const signup = user => dispatch => {
+  return SessionAPIUtil.signup(user)
+    .then((res) => dispatch(receiveCurrentUser(res)),
+      (err) => dispatch(receiveErrors(err.responseJSON)));
+};
---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,12 @@
+import { RECEIVE_CURRENT_USER, RECEIVE_ERRORS } from '../actions/session_actions';
+
+const _defaultState = {
+  currentUser: null,
+  errors: []
+};
+
+const SessionReducer = (oldState={}, action) => {
+
+};
+
+export default SessionReducer;
