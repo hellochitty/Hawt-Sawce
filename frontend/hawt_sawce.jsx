52ead39892627f21768d0b50bbbc0671d29b964e
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-04-18 15:45:54 -0400

    Finish root and app

--- frontend/hawt_sawce.jsx
+++ frontend/hawt_sawce.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from './components/root';
 import { login, logout, signup } from './util/session_api_util';
-import configureStore from './store/store.js';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', ()=> {
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>hello</h1>,root);
+  const store = configureStore();
+  ReactDOM.render(<Root store={ store } />, root);
   window.login = login;
   window.logout = logout;
   window.signup = signup;
