1658830060ce9025ba11d2512994805939e52480
chithra <chithra.venkatesan.5@gmail.com>
2017-04-19 11:33:36 -0400

    Add guest login

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -11,6 +11,7 @@ class SessionForm extends React.Component {
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.update = this.update.bind(this);
+    this.guestLogin = this.guestLogin.bind(this);
   }
 
   componentWillUnmount(){
@@ -21,6 +22,11 @@ class SessionForm extends React.Component {
     if(this.props.processForm &&
       (this.props.processForm !== newProps.processForm)){
       this.props.clearErrors();
+      this.setState({
+        username: '',
+        password: '',
+        email: ''
+      });
     }
   }
 
@@ -40,6 +46,7 @@ class SessionForm extends React.Component {
     };
   }
 
+  //show errors per error item
   loginErrors(){
     if (this.props.errors) {
       if (this.props.errors.login){
@@ -69,15 +76,22 @@ class SessionForm extends React.Component {
       }
     }
   }
-
-
+  //guest login
+  guestLogin(){
+    this.setState({
+      username: 'chithra',
+      password: 'password',
+    });
+  }
   render(){
     const text = this.props.formType === 'login' ? "Log In" : "Sign Up";
     const link = this.props.formType === 'login' ? "/signup" : "/login";
     let emailInput = null;
     if (this.props.formType !== 'login'){
-      emailInput = (<section>Email: <input type='text' value={this.state.email} onChange={this.update('email')} />
-    <p className="session-errors">{ this.emailErrors()}</p></section>);
+      emailInput = (
+      <section>Email: <input type='text' value={this.state.email} onChange={this.update('email')} />
+      <p className="session-errors">{ this.props.errors.email}</p>
+      </section>);
     }
 
     return(
@@ -85,15 +99,16 @@ class SessionForm extends React.Component {
         <h2>{text}</h2>
         <form onSubmit={this.handleSubmit}>
           <section>Username: <input type='text' value={this.state.username} onChange={this.update('username')} />
-          <p className="session-errors">{ this.usernameErrors()}</p></section>
+          <p className="session-errors">{ this.props.errors.username}</p></section>
 
           <section>Password: <input type='password' value={this.state.password} onChange={this.update('password')} />
-          <p className="session-errors">{ this.passwordErrors()}</p></section>
+          <p className="session-errors">{ this.props.errors.password}</p></section>
 
           {emailInput}
-          <section><p className="session-errors">{ this.loginErrors()}</p></section>
+          <section><p className="session-errors">{ this.props.errors.login}</p></section>
           <input type='submit' value='submit' />
         </form>
+        <button onClick={this.guestLogin}>Guest</button>
         <Link to={link}>{link.slice(1)} </Link>
       </div>
     );
