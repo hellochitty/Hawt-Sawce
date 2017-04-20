75252ce79737b328d05e6f28c5ee9d424c66cf66
chithra <chithra.venkatesan.5@gmail.com>
2017-04-20 10:36:25 -0400

    Add image to sauce view and sauce component

--- app/views/api/sauces/_sauce.json.jbuilder
+++ app/views/api/sauces/_sauce.json.jbuilder
@@ -1,4 +1,4 @@
 json.extract! sauce, :id, :name, :description, :scoville_units
 json.company  sauce.company.name
-# json.url :image_url,
+json.image_url sauce.image_url
 # json.company do sauce.company.name end
--- frontend/components/sauce/sauce_index_item.jsx
+++ frontend/components/sauce/sauce_index_item.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 const SauceIndexItem = ({sauce}) => (
-  <div id='sauce-index-item'>
+  <div className='sauce-index-item'>
     <h1>{sauce.name}</h1>
     <h2>{sauce.company}</h2>
+    <h3>{sauce.scoville_units} SHU</h3>
     <h4>{sauce.description}</h4>
+    <img className='sauce-thumbnail' src={sauce.image_url} />
   </div>
 );
 
