0a729a1846ae7286b8f1515ee44f47e77d0cc6ae
chithra <chithra.venkatesan.5@gmail.com>
2017-05-07 12:18:06 -0400

    Remove minification warning and add redux logger only to dev

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,15 +1,27 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import RootReducer from '../reducers/root_reducer';
-import { createLogger } from 'redux-logger';
-const logger = createLogger();
+// import { createLogger } from 'redux-logger';
+// const logger = createLogger();
 
-const configureStore = (preloadedState={}) => (
-  createStore(
-    RootReducer,
-    preloadedState,
-    applyMiddleware(thunk, logger)
-  )
+// const configureStore = (preloadedState={}) => (
+//   createStore(
+//     RootReducer,
+//     preloadedState,
+//     applyMiddleware(thunk, logger)
+//   )
+// );
+
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  // must use 'require' (import only allowed at top of file)
+  const createLogger = require('redux-logger');
+  middlewares.push(createLogger());
+}
+
+const configureStore = (preloadedState = {}) => (
+  createStore(RootReducer, preloadedState, applyMiddleware(...middlewares))
 );
 
 export default configureStore;
